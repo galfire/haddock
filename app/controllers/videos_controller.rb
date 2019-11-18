ea2dd596d33a9513a351a2a94237fefea2464715
Brandon Vrooman <brandon@galfire.tv>
2019-11-18 17:47:10 -0500

    Enable iframe requests (#11)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,3 +1,9 @@
 class VideosController < ApplicationController
+  before_action :allow_iframe_requests
+
+  def allow_iframe_requests
+    response.headers.delete :"X-Frame-Options"
+  end
+
   def show; end
 end
